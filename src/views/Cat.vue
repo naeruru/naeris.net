bdd1b6c630f78a72d62b9f4ce7a0c01b37248734
nae <nae@risu.dev>
2024-02-08 16:39:46 -0600

    update grabbable css

--- src/views/Cat.vue
+++ src/views/Cat.vue
@@ -3,7 +3,7 @@
     <v-card-text class="text-subtitle-1 py-2 my-4">
       <p>Here is my cat ˆ. ̫ .ˆ₎</p>
     </v-card-text>
-    <v-img class="mx-auto" style="{ cursor: move; cursor: grab; }" :width="500" src="@/assets/cat.jpg">
+    <v-img class="mx-auto grabbable" :width="500" src="@/assets/cat.jpg">
       <template v-slot:placeholder>
         <div class="d-flex align-center justify-center fill-height">
           <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
@@ -18,3 +18,17 @@
 </script>
 
 
+<style>
+.grabbable {
+    cursor: move;
+    cursor: grab;
+    cursor: -moz-grab;
+    cursor: -webkit-grab;
+}
+
+.grabbable:active {
+    cursor: grabbing;
+    cursor: -moz-grabbing;
+    cursor: -webkit-grabbing;
+}
+</style>
