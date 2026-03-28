740afcb6120da2dc474bfae8d652d9a53cf5d466
nae <nae@risu.dev>
2026-03-28 16:22:25 -0500

    greatly simplify plane creation

--- src/views/Cat.vue
+++ src/views/Cat.vue
@@ -26,6 +26,8 @@
   import * as THREE from 'three'
   // @ts-expect-error
   import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+  // @ts-expect-error
+  import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
   import cat1 from '@/assets/cat.png'
   import cardback from '@/assets/cardback.png'
@@ -55,12 +57,14 @@
   //   .onChange(genPlane)
 
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000) // (FOV, ASPECT RATIO, near clipping plane, far clipping plane)
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000) // (FOV, ASPECT RATIO, near clipping plane, far clipping plane)
   const renderer = new THREE.WebGLRenderer({ alpha: true })
 
+  const loader = new GLTFLoader()
+
   // set renderer
-  renderer.setSize(innerWidth - marginX, innerHeight - marginY)
-  renderer.setPixelRatio(devicePixelRatio)
+  renderer.setSize(window.innerWidth - marginX, window.innerHeight - marginY)
+  renderer.setPixelRatio(window.devicePixelRatio)
 
   // set controls + camera pos
   const controls = new OrbitControls(camera, renderer.domElement)
@@ -74,7 +78,8 @@
     color: 0xffffff,
     // side: THREE.DoubleSide,
     flatShading: true,
-    shininess: 100
+    shininess: 100,
+    transparent: true,
   }
 
   const texture = new THREE.TextureLoader().load(cat1)
@@ -84,21 +89,24 @@
   const backMat = new THREE.MeshPhongMaterial({ ...basicMatProps, map: textureBack, })
 
   // create double-sided plane
+  // const cardMesh2 = new THREE.Mesh(
+  //   new THREE.PlaneGeometry(1, 1)
+  // )
   var cardMesh = new THREE.Mesh(
-    RectangleRounded(
+    new THREE.PlaneGeometry(
       world.plane.width * world.plane.size,
       world.plane.height * world.plane.size,
-      .05,
-      5,
+      // .05,
+      // 5,
       // .5
     ), frontMat,
   )
   var cardMeshBack = new THREE.Mesh(
-    RectangleRounded(
+    new THREE.PlaneGeometry(
       world.plane.width * world.plane.size,
       world.plane.height * world.plane.size,
-      .05,
-      5,
+      // .05,
+      // 5,
       // .5
     ), backMat,
   ).rotateY(Math.PI)
@@ -106,6 +114,7 @@
   // add to scene
   scene.add(cardMesh)
   scene.add(cardMeshBack)
+  // scene.add(cardMesh2)
 
   // create light
   const light = new THREE.DirectionalLight(0xFFFFFF, 2.5) // color, intensity
@@ -134,52 +143,12 @@
   //   )
   // }
 
-  // generate rounded plane
-  function RectangleRounded(w: number, h: number, r: number, s: number) {
-    const pi2 = Math.PI * 2
-    const n = (s + 1) * 4 // number of segments
-    let indices = []
-    let positions = []
-    let uvs = []
-    let qu, sgx, sgy, x, y
-
-    for (let j = 1; j < n + 1; j++) indices.push(0, j, j + 1) // 0 is center
-    indices.push(0, n, 1)
-    positions.push(0, 0, 0) // rectangle center
-    uvs.push(0.5, 0.5)
-    for (let j = 0; j < n; j++) contour(j)
-
-    const geometry = new THREE.BufferGeometry();
-    geometry.setIndex(new THREE.BufferAttribute(new Uint32Array(indices), 1));
-    geometry.setAttribute(
-      "position",
-      new THREE.BufferAttribute(new Float32Array(positions), 3)
-    )
-    geometry.setAttribute(
-      "uv",
-      new THREE.BufferAttribute(new Float32Array(uvs), 2)
-    )
-
-    return geometry
-
-    function contour(j: number) {
-      qu = Math.trunc((4 * j) / n) + 1 // quadrant  qu: 1..4
-      sgx = qu === 1 || qu === 4 ? 1 : -1 // signum left/right
-      sgy = qu < 3 ? 1 : -1; // signum  top / bottom
-      x = sgx * (w / 2 - r) + r * Math.cos((pi2 * (j - qu + 1)) / (n - 4)) // corner center + circle
-      y = sgy * (h / 2 - r) + r * Math.sin((pi2 * (j - qu + 1)) / (n - 4))
-
-      positions.push(x, y, 0)
-      uvs.push(0.5 + x / w, 0.5 + y / h)
-    }
-  }
-
   // on size window
   function onResize () {
     windowSize.value = { x: window.innerWidth, y: window.innerHeight }
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
-    renderer.setSize(innerWidth - marginX, innerHeight - marginY)
+    renderer.setSize(window.innerWidth - marginX, window.innerHeight - marginY)
   }
 
   // mount start
